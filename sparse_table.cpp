fcced36e33fc19d07ac2b9c2933f1bff68ca7e50
Mudit Rastogi <72215754+MuditRastogi1@users.noreply.github.com>
2025-02-16 16:59:46 +0530

    Add files via upload

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,51 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-const int MAX_N = 100'00'06;
-const int LOG = 20;
-int sparse[MAX_N][LOG]; // sparse[i][j] is minimum among arr[i..i+2^j-1]
-int bin_log[MAX_N];
 
-void build(int n)
-{
-       for(int j=1; j<LOG; j++) {
-          for(int i=0; i+(1<<j)-1<n; i++) {
-             sparse[i][j] = min(sparse[i][j-1],sparse[i+(1<<(j-1))][j-1]);
+class SparseTable {
+    vector<vector<int>> sparse; // sparse[i][j] is operation among arr[i..i+2^j-1]
+    vector<int> bin_log;
+
+    public:
+    SparseTable(int n) {
+        sparse.resize(n, vector<int>(20));
+        bin_log.resize(n + 1);
+        bin_log[1] = 0; //for quickly finding log2()
+
+        for(int i = 2; i <= n; i++) {
+            bin_log[i] = bin_log[i/2]+1;
         }
     }
-}
 
-int query(int l, int r)
-{
-    int len = r - l + 1;
-    int k = bin_log[len]; // while((1<<(k+1)) <= len) k++;
- return min(sparse[l][k],sparse[r-(1<<k)+1][k]);
-}
+    int combine(int a, int b) {
+        return max(a, b); //only for idempotent functions (min, max, gcd, lcm)
+    }
+
+    void build(vector<int>& arr) {
+        for(int i=0; i<int(arr.size()); i++) {
+            sparse[i][0] = arr[i];
+        }
+
+        for(int j=1; j<20; j++) {
+            for(int i=0; i+(1<<j)-1<int(arr.size()); i++) {
+                sparse[i][j] = combine(sparse[i][j-1],sparse[i+(1<<(j-1))][j-1]);
+            }
+        }
+    }
+
+    int query(int l, int r) {
+        int len = r - l + 1;
+        int k = bin_log[len]; // while((1<<(k+1)) <= len) k++;
+        return combine(sparse[l][k],sparse[r-(1<<k)+1][k]);
+    }
+};
 
 int main()
 {
     int n,q;
-    cin>>n>>q;
-    int arr[n];
-
-	bin_log[1] = 0; //for quickly finding log2()
-	for(int i = 2; i <= n; i++) {
-		bin_log[i] = bin_log[i/2]+1;
-	}
+    cin >> n >> q;
+    vector<int> arr(n);
 
     for(int i=0; i<n; i++) {
-       cin>>arr[i];
-       sparse[i][0] = arr[i];
+        cin >> arr[i];
     }
 
+    SparseTable st(n);
     //pre-processing
-    build(n);
+    st.build(arr);
 
     //answer queries in O(1)
     for(int i=0; i<q; i++) {
         int l,r;
         cin>>l>>r;
-        cout<<query(l-1,r-1)<<endl;
+        cout<<st.query(l,r)<<endl;
     }
     return 0;
 }
