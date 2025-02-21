74b8ba20b4173d7503ef9e07e38076892b21000b
Mudit Rastogi <72215754+MuditRastogi1@users.noreply.github.com>
2025-02-21 17:06:09 +0530

    Update fenwick_tree.cpp

--- fenwick_tree.cpp
+++ fenwick_tree.cpp
@@ -1,23 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class FenwickTree
-{
+class FenwickTree {
     vector<int> BIT;
     public:
     FenwickTree(int n) {
         BIT.resize(n+1);
     }
 
-    void update(int i, int delta)
-    {
+    void update(int i, int delta) {
         for(; i<int(BIT.size()); i += (i&-i)) {
             BIT[i] += delta;
         }
     }
 
-    int query(int i)
-    {
+    int query(int i) {
         int sum = 0;
         for(; i>0; i -= (i&-i)) {
             sum += BIT[i];
@@ -42,52 +39,14 @@ class FenwickTree
     }
 };
 
-vector<int> solve(vector<int>& a, vector<vector<int>>& query) {
-    FenwickTree ft(a.size()+2);
-    vector<int> ans;
-    for(int i=0; i<a.size(); i++) {
-        ft.update(i+1,a[i]);
-    }
-
-    for(auto q : query) {
-        if(q[0] == 1) {
-            ft.update(q[1],-a[q[1]-1]+q[2]);
-            a[q[1]-1] = q[2];
-        }
-        else {
-            int l = q[1], r = q[2];
-            int curr = 0;
-            for(int i=l; i<=r; i++) {
-                for(int j=i; j<=r; j++) {
-                    curr += (ft.query(j) - ft.query(i-1));
-                }
-            }
-            ans.push_back(curr);
-        }
-    }
- return ans;
-}
-
-int main()
-{
+int main() {
     vector<vector<int>> query = {{1,2,2},{2,1,3}};
     vector<int> a = {2,1,4,3,1};
-    vector<int> ans = solve(a,query);
 
-    for(auto it : ans) cout<<it<<" ";
-    // FenwickTree ft(4);
-    // for(int i=0; i<4; i++) {
-    //     ft.update(i+1,arr[i]);
-    //     cout<<ft.query(i+1)<<" ";
-    // }
-
-    //create(arr);
-
-    // arr = compressCordinates(arr);
-    // for(auto it : arr) {
-    //     cout<<it<<" ";
-    // }
-    
-    // cout<<query(5);
-    return 0;
+    FenwickTree ft(a.size());
+    for(int i=0; i<4; i++) {
+        ft.update(i+1, arr[i]);
+        cout << ft.query(i+1) << " ";
+    }
+ return 0;
 }
