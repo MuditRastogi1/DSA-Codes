2d30acc3890635386e062f10fd86cdcd02893da7
Mudit Rastogi <72215754+MuditRastogi1@users.noreply.github.com>
2025-04-24 23:53:03 +0530

    Update segment_tree.cpp

--- segment_tree.cpp
+++ segment_tree.cpp
@@ -2,67 +2,66 @@
 using namespace std;
  
 class SegmentTree {
-  int len;
-  vector<int> seg;
+    int len;
+    vector<int> seg;
 
-   void combine(int ind) { 
-	   seg[ind] = seg[2*ind+1] + seg[2*ind+2]; 
-   }
-   
+    void combine(int ind) { 
+        seg[ind] = seg[2 * ind + 1] + seg[2 * ind + 2]; 
+    }
+
     void build(int ind, int low, int high, vector<int>& arr) {
         if(low == high) {
             seg[ind] = arr[low];
             return;
-         }
-      int mid = (low + high)/2;
-      build(2*ind+1,low,mid,arr);
-      build(2*ind+2,mid+1,high,arr);
-      combine(ind);
-    }
-
-    int query(int ind, int low, int high, int l, int r) {
-      if(low > r || high < l)
-        return 0;
-                                  
-      if(l <= low && high <= r)
-        return seg[ind];
-      
-      int mid = (low + high)/2;
-      int left = query(2*ind+1,low,mid,l,r);
-      int right = query(2*ind+2,mid+1,high,l,r);
-      return left + right;
-    }
-
-    void update(int ind, int low, int high, int index, int val) {
-        if(low == high) {
-            seg[ind] = val;
-            return;
         }
-        int mid = (low + high)/2;
-        if(index <= mid) {
-          update(2*ind+1,low,mid,index,val);
-	}
-        else {
-         update(2*ind+2,mid+1,high,index,val);
-	}
+        int mid = (low + high) / 2;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
         combine(ind);
     }
 
-    public:
+    int query(int ind, int low, int high, int l, int r) {
+        if (low > r || high < l)
+            return 0;
+
+        if (l <= low && high <= r)
+            return seg[ind];
+
+        int mid = (low + high) / 2;
+        int left = query(2 * ind + 1, low, mid, l, r);
+        int right = query(2 * ind + 2, mid + 1, high, l, r);
+        return left + right;
+    }
+
+    void update(int ind, int low, int high, int index, int val) {
+        if (low == high) {
+            seg[ind] = val;
+            return;
+        }
+        int mid = (low + high) / 2;
+        if(index <= mid) {
+            update(2 * ind + 1, low, mid, index, val);
+        } else {
+            update(2 * ind + 2, mid + 1, high, index, val);
+        }
+        combine(ind);
+    }
+
+public:
     SegmentTree(int n) {
-      len = n;
-      seg.resize(4*len+1);
+        len = n;
+        seg.resize(4 * len + 1);
     }
 
-    void build(vector<int>& arr) {
-      build(0,0,len-1,arr);
+    void build(vector<int>& arr) { 
+        build(0, 0, len - 1, arr); 
     }
 
-    void update(int index, int val) {
-      update(0,0,len-1,index,val);
+    void update(int index, int val) { 
+        update(0, 0, len - 1, index, val); 
     }
 
-    int query(int l, int r) {
-      return query(0,0,len-1,l,r);
+    int query(int l, int r) { 
+        return query(0, 0, len - 1, l, r); 
     }
 };
